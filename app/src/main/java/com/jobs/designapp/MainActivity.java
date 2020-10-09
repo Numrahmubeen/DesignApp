080398194d9358caf45204d8e41b43ea7a1977a4
Numrahmubeen <numrahmubeen@gmail.com>
2020-10-09 20:54:13 +0500

    Initial commit

---
+++ app/src/main/java/com/jobs/designapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jobs.designapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager=findViewById(R.id.viewpager);
+    }
+}
